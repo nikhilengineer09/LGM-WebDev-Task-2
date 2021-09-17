277d54e1523c8916419686d12bca9c862931f1ef
Nikhil <nikhil.joshi1309@gmail.com>
2021-09-17 22:45:23 +0530

    Initial Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { apifetch } from "./component/1";
+import  Navbar  from "./component/2";
+import  Users  from "./component/3";
+import { Loader } from "./component/4";
+import './App.css';
+
+function App() {
+   const [personData,setpersonData] = useState([]);
+   const [loading,setLoading] = useState(false);
+   const [page,setPage] = useState({minPage:1, maxPage:6});
+   const personDataPresent = personData.length !== 0;
+
+   const getpersonData  = async minPage => {
+     if( personData.length !==0 && minPage === 1){
+       return;
+     }
+     setLoading(true);
+     const users = await apifetch(minPage);
+     setPage({ minPage, maxPage: personData.total_pages});
+
+     setLoading(false);
+     setpersonData([...personData,...users.data]);
+     
+   };
+  
+ 
+
+  return(
+    <div className="App">
+     <Navbar getpersonData={ getpersonData }/>
+     { !loading && !personDataPresent && (
+       <div className="container">
+      
+       </div>
+       
+     )}
+
+      {loading && (
+        <Loader/>
+      )}
+
+      { !loading && personData && <Users personData={personData}/>}
+
+    </div>
+  );
+}
+
+export default App;
